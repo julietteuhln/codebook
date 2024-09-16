0e450d8b1f3d56a89b493ab0699f554f8d8583a0
julietteuhln <julietteuhlen@gmail.com>
2024-09-16 16:26:44 +0200

    Update environment.prod.ts

--- codebook-admin/codebook-admin/src/environments/environment.prod.ts
+++ codebook-admin/codebook-admin/src/environments/environment.prod.ts
@@ -1,13 +1,1 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyA0667VyrCUHNjkooD5_hJSDD2M6qaEal4",
-        authDomain: "codebook-79011.firebaseapp.com",
-        databaseURL: "https://codebook-79011-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "codebook-79011",
-        storageBucket: "codebook-79011.appspot.com",
-        messagingSenderId: "340387959983",
-        appId: "1:340387959983:web:a059b606b4c4597a83b05a"
-    }
-};
-    
+
